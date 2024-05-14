158ded4d5b07359522420b840019420c52bdd133
Joel West <emailjoelwest@gmail.com>
2024-05-14 15:41:28 +1000

    Oops forgot to include parser in previous commit

---
+++ propnet/basenet_parser.cpp
@@ -0,0 +1,238 @@
+#include "include/basenet_parser.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+namespace propnet
+{
+    /*
+    Must be kept in line with the values
+    in the Python file games/constants.py
+    (which then get outputted to the json variants)
+    */
+    enum EntryType
+    {
+        And = 1,
+        Or = 2,
+        Proposition = 3,
+        Not = 5,
+        Constant = 6,
+        PreTransition = 8,
+        PostTransition = 9,
+    };
+
+    ParsingError::ParsingError(const char* message) :
+        std::runtime_error {message}
+    {}
+
+    /*
+    Not optimised as this isn't a critical
+    section, and is only done once.
+
+    Also kept in a single function to avoid exposing
+    the json library to above files (and to avoid creating
+    a lot of cluttered classes).
+
+    Completely terrible and unreadable, but gets
+    the job done and I don't wanna spend any more
+    time than needed parsing stuff in C++.
+    */
+    BaseNetParser::BaseNetParser(std::string_view game)
+    {
+        std::filesystem::path game_path {GAMES_PATH};
+        game_path.append(game);
+        game_path.concat(".");
+        game_path.concat(JSON_EXTENSION);
+
+        std::ifstream game_file {game_path};
+        if (!game_file.good())
+        {
+            throw ParsingError {"Could not find given game in json format"};
+        }
+
+        try
+        {
+            const auto game_json = nlohmann::json::parse(game_file);
+
+            const auto& role_entries {game_json.at(ROLES_KEY)};
+            for (const auto& role_entry : role_entries)
+            {
+                roles.emplace_back(
+                    role_entry.at(ROLE_KEY).get<std::string>(),
+                    role_entry.at(SEES_KEY).get<std::vector<std::uint32_t>>(),
+                    role_entry.at(LEGALS_KEY).get<std::vector<std::uint32_t>>()
+                );
+            }
+
+            const auto& entries {game_json.at(ENTRIES_KEY)};
+            for (const auto& entry : entries)
+            {
+                add_entry(entry);
+            }
+
+            topologically_sorted_nodes = game_json.at(TOPOLOGICALLY_SORTED_KEY).get<std::vector<std::uint32_t>>();
+
+            std::cout << game_json.at(LEGAL_TO_INPUT_KEY) << '\n';
+            // legal_to_input = game_json.at(LEGAL_TO_INPUT_KEY).get<std::vector<std::unordered_map<std::uint32_t, std::uint32_t>>>();
+        }
+        catch (const nlohmann::json::exception& error)
+        {
+            throw ParsingError {error.what()};
+        }
+
+
+        if (terminal == nullptr)
+        {
+            throw ParsingError {"No terminal state found in definition"};
+        }
+    }
+
+    BaseNet BaseNetParser::create_basenet()
+    {
+        if (!is_data_parsed)
+        {
+            throw std::logic_error {"Tried to create more than one basenet from same parser"};
+        }
+
+        is_data_parsed = false;
+        return BaseNet {
+            std::move(roles),
+            std::move(nodes),
+            std::move(terminal),
+            std::move(topologically_sorted_nodes)
+        };
+    }
+
+    void BaseNetParser::add_entry(const nlohmann::json& entry)
+    {
+        const std::uint32_t id {entry.at(ID_KEY)};
+        const EntryType entry_type {entry.at(TYPE_KEY)};
+        switch (entry_type)
+        {
+            case EntryType::And:
+                add_node(AndNode {
+                    id,
+                    entry.at(INS_PROPS_KEY)
+                });
+                break;
+            case EntryType::Or:
+                add_node(OrNode {
+                    id,
+                    entry.at(INS_PROPS_KEY)
+                });
+                break;
+            case EntryType::Proposition:
+                add_proposition(
+                    id,
+                    entry.at(PROP_TYPE_KEY).get<std::string>(),
+                    entry.at(GDL_KEY).get<std::string>(),
+                    entry
+                );
+                break;
+            case EntryType::PreTransition:
+                add_node(PreTransitionNode {
+                    id,
+                    entry.at(IN_PROPS_KEY)
+                });
+                break;
+            case EntryType::PostTransition:
+                add_node(PostTransitionNode {
+                    id,
+                    entry.at(IN_PROPS_KEY)
+                });
+                topologically_sorted_nodes.push_back(id);
+                break;
+            case EntryType::Not:
+                add_node(NotNode {
+                    id,
+                    entry.at(IN_PROPS_KEY)
+                });
+                break;
+            case EntryType::Constant:
+                add_node(TrueNode {
+                    id
+                });
+                break;
+            default:
+                throw ParsingError {"Unknown entry type found"};
+        }
+    }
+
+    void BaseNetParser::add_proposition(std::uint32_t id, std::string_view type, std::string&& gdl, const nlohmann::json& entry)
+    {
+        // TODO: Repetitive, but it works for now at least
+        if (type == INITIAL_PROP_TYPE)
+        {
+            add_node(InitialPropositionNode {
+                id,
+                gdl
+            });
+        }
+        else if (type == BASE_PROP_TYPE)
+        {
+            add_node(BasicPropositionNode {
+                id,
+                gdl,
+                entry.at(IN_PROPS_KEY)
+            });
+        }
+        else if (type == INPUT_PROP_TYPE)
+        {
+            add_node(InitialPropositionNode {
+                id,
+                gdl,
+            });
+        }
+        else if (type == LEGAL_PROP_TYPE)
+        {
+            add_node(BasicPropositionNode {
+                id,
+                gdl,
+                entry.at(IN_PROPS_KEY)
+            });
+        }
+        else if (type == GOAL_PROP_TYPE)
+        {
+            add_node(BasicPropositionNode {
+                id,
+                gdl,
+                entry.at(IN_PROPS_KEY)
+            });
+        }
+        else if (type == SEES_PROP_TYPE)
+        {
+            add_node(BasicPropositionNode {
+                id,
+                gdl,
+                entry.at(IN_PROPS_KEY)
+            });
+        }
+        else if (type == TERMINAL_PROP_TYPE)
+        {
+            if (terminal != nullptr)
+            {
+                throw ParsingError {"More than one terminal state found whilst parsing"};
+            }
+
+            terminal = std::make_unique<BasicPropositionNode>(
+                id,
+                entry.at(GDL_KEY).get<std::string>(),
+                entry.at(IN_PROPS_KEY)
+            );
+            nodes.push_back(terminal);
+        }
+        else if (type == OTHER_PROP_TYPE)
+        {
+            add_node(BasicPropositionNode {
+                id,
+                entry.at(GDL_KEY).get<std::string>(),
+                entry.at(IN_PROPS_KEY)
+            });
+        }
+        else
+        {
+            throw ParsingError {"Got unknown proposition type"};
+        }
+    }
+};
---
+++ propnet/include/basenet_parser.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "basenet.hpp"
+
+#include <nlohmann/json.hpp>
+#include <concepts>
+#include <stdexcept>
+#include <string_view>
+
+namespace propnet
+{
+    class ParsingError : public std::runtime_error
+    {
+        public:
+            ParsingError(const char* message);
+    };
+
+    class BaseNetParser
+    {
+        public:
+            BaseNetParser(std::string_view game);
+
+            BaseNet create_basenet();
+        private:
+            static constexpr auto GAMES_PATH {"games/json/"};
+            static constexpr auto JSON_EXTENSION {"json"};
+
+            static constexpr auto ROLES_KEY {"roles"};
+            static constexpr auto ROLE_KEY {"role"};
+            static constexpr auto SEES_KEY {"sees"};
+            static constexpr auto LEGALS_KEY {"legals"};
+            static constexpr auto ENTRIES_KEY {"entries"};
+            static constexpr auto ID_KEY {"id"};
+            static constexpr auto TYPE_KEY {"type"};
+            static constexpr auto IN_PROPS_KEY {"in"};
+            static constexpr auto INS_PROPS_KEY {"ins"};
+            static constexpr auto PROP_TYPE_KEY {"proposition_type"};
+            static constexpr auto GDL_KEY {"gdl"};
+            static constexpr auto VALUE_KEY {"value"};
+            static constexpr auto TOPOLOGICALLY_SORTED_KEY {"topologically_sorted"};
+            static constexpr auto LEGAL_TO_INPUT_KEY {"legal_to_input"};
+
+            static constexpr auto INITIAL_PROP_TYPE {"init"};
+            static constexpr auto BASE_PROP_TYPE {"base"};
+            static constexpr auto INPUT_PROP_TYPE {"input"};
+            static constexpr auto LEGAL_PROP_TYPE {"legal"};
+            static constexpr auto GOAL_PROP_TYPE {"goal"};
+            static constexpr auto SEES_PROP_TYPE {"sees"};
+            static constexpr auto TERMINAL_PROP_TYPE {"terminal"};
+            static constexpr auto OTHER_PROP_TYPE {"other"};
+
+            template<std::derived_from<Node> T>
+            void add_node(T node)
+            {
+                nodes.emplace_back(std::make_unique<const T>(node));
+            }
+
+            void add_entry(const nlohmann::json& entry);
+            void add_proposition(std::uint32_t id, std::string_view type, std::string&& gdl, const nlohmann::json& entry);
+
+            bool is_data_parsed {true};
+            std::vector<Role> roles {};
+            std::vector<std::shared_ptr<const Node>> nodes {};
+            std::shared_ptr<const Node> terminal {nullptr};
+            std::vector<std::uint32_t> topologically_sorted_nodes {};
+            std::vector<std::unordered_map<std::uint32_t, std::uint32_t>> legal_to_input {};
+    };
+};
